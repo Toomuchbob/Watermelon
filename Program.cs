b7e6c028a6c28979911c3b89906c0b41a2c8af1f
Gino Zaccardelli <ginozacc@gmail.com>
2022-05-16 11:22:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord.Addons.Hosting;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Watermelon.Services;
+
+namespace Watermelon
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(app =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true)
+                        .Build();
+
+                    app.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                })
+                .ConfigureDiscordHost((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Debug,
+                        AlwaysDownloadUsers = false,
+                        MessageCacheSize = 200,
+                    };
+
+                    config.Token = context.Configuration["Token"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = LogSeverity.Debug;
+                    config.DefaultRunMode = RunMode.Sync;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services
+                    .AddHostedService<CommandHandler>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
---
+++ Services/CommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using System.Reflection;
+
+namespace Watermelon.Services
+{
+    public class CommandHandler : DiscordClientService
+    {
+        private readonly IServiceProvider _provider;
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _service;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+
+        public CommandHandler(
+            IServiceProvider serviceProvider,
+            DiscordSocketClient client,
+            CommandService service,
+            IConfiguration configuration,
+            ILogger<DiscordClientService> logger
+            ) : base(client, logger)
+        {
+            _provider = serviceProvider;
+            _client = client;
+            _service = service;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _client.MessageReceived += OnMessageReceived;
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+
+        private async Task OnMessageReceived(SocketMessage socketMessage)
+        {
+            if (!(socketMessage is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            var argPos = 0; // looks for the prefix of the message command as detailed in the config
+            if (!message.HasStringPrefix(_configuration["Prefix"], ref argPos) && !message.HasMentionPrefix(_client.CurrentUser, ref argPos)) return;
+
+            var context = new SocketCommandContext(_client, message);
+            await _service.ExecuteAsync(context, argPos, _provider);
+        }
+    }
+}
